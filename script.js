a8a3388a4f463257d7f07cc267fe94c72ff8a578
Bertrand Surjous <body.bertrand@gmail.com>
2024-01-11 13:12:24 +0100

    premieres consignes

---
+++ script.js
@@ -0,0 +1,23 @@
+const shebang = [...document.querySelectorAll('.box')];
+console.log(shebang);
+
+const screenboard = document.querySelector('.quijoue');
+console.log(screenboard);
+let scoreplayer1 = document.querySelector('.score1');
+console.log(scoreplayer1);
+let scoreplayer2 = document.querySelector('.score2');
+console.log(scoreplayer2);
+let egality = document.querySelector('.scorenul');
+console.log(egality);
+
+//--------------------------------Selecteurs--------------------------------------------------------
+shebang.forEach(e => {
+    e.addEventListener('click',theGame)
+    
+});
+
+//------------------------------Structure du jeu------------------------------------------------
+
+function theGame(e){
+    console.log(e);
+}
